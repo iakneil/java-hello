dd02875042ad251492822f2013c40f23f56e1d30
qwn <37524022@qq.com>
2017-07-16 23:55:36 +0800

    to root

--- src/main/java/spring/demo/service/DemoService.java
+++ src/main/java/spring/demo/service/DemoService.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Service;
 public class DemoService {
 
 	public String Verify(String username,String password){
-		
-		if(username.equals("admin") && password.equals("admin")){
+
+		if(username.equals("root") && password.equals("root")){
 			return "Login Success！";
 		}else{
 			return "Login Failed,Please Try Again！";
